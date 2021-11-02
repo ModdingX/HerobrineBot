64aeba55633e3d36eb1b442a2b2fe022131993cd
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-11-02 22:28:28 +0100

    Remove print

--- discordbot/discordReaction.ts
+++ discordbot/discordReaction.ts
@@ -2,19 +2,21 @@ import {Client as DiscordClient, Guild, GuildMember, Message, MessageReaction, P
 
 export async function addReactionRole(discord: DiscordClient, guild: Guild, roleMessage: Message, emote: string, roleId: string) {
     const role = await guild.roles.fetch(roleId)
-    console.log(role)
-    
-    discord.on("messageReactionAdd", async (reaction: MessageReaction, user: User | PartialUser) => {
-        if (emote == reaction.emoji.id && reaction.message.id == roleMessage.id && reaction.message.guild != null) {
-            let member: GuildMember = await reaction.message.guild.members.fetch(user.id);
-            await member.roles.add(roleId);
-        }
-    })
+    if (role == undefined) {
+        console.log("Role not found: " + roleId)
+    } else {
+        discord.on("messageReactionAdd", async (reaction: MessageReaction, user: User | PartialUser) => {
+            if (emote == reaction.emoji.id && reaction.message.id == roleMessage.id && reaction.message.guild != null) {
+                let member: GuildMember = await reaction.message.guild.members.fetch(user.id);
+                await member.roles.add(role.id);
+            }
+        })
 
-    discord.on("messageReactionRemove", async (reaction: MessageReaction, user: User | PartialUser) => {
-        if (emote == reaction.emoji.id && reaction.message.id == roleMessage.id && reaction.message.guild != null) {
-            let member: GuildMember = await reaction.message.guild.members.fetch(user.id);
-            await member.roles.remove(roleId);
-        }
-    })
+        discord.on("messageReactionRemove", async (reaction: MessageReaction, user: User | PartialUser) => {
+            if (emote == reaction.emoji.id && reaction.message.id == roleMessage.id && reaction.message.guild != null) {
+                let member: GuildMember = await reaction.message.guild.members.fetch(user.id);
+                await member.roles.remove(role.id);
+            }
+        })
+    }
 }
