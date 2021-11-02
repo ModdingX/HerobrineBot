9dcf275ad1b9c44e58107a32dd7be650e2915d1a
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-11-02 22:26:44 +0100

    Remove useless error

--- discordbot/discordBot.ts
+++ discordbot/discordBot.ts
@@ -5,9 +5,6 @@ import {addReactionRole} from "./discordReaction";
 
 
 export async function startDiscordBot(discord: DiscordClient, config: BotConfig): Promise<void> {
-    if (process.env.DISCORD_GUILD == null) {
-        throw new Error("No guild given")
-    }
     const guild: Guild = await discord.guilds.fetch(config.guild);
     const roleChannel = await textChannel(discord, config.role_channel);
     const roleMessage = await roleChannel.messages.fetch(config.role_message)
